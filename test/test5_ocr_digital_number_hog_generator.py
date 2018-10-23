bb43083bfd29c51c17e6f3de13b9412b5f64f687
hequn <hequn0815@gmail.com>
2018-10-23 11:57:37 +0800

    OCR HOG generator and recognition

---
+++ test/test5_ocr_digital_number_hog_generator.py
@@ -0,0 +1,34 @@
+# Import the modules
+from sklearn.externals import joblib
+from sklearn import datasets
+from skimage.feature import hog
+from sklearn.svm import LinearSVC
+import numpy as np
+from collections import Counter
+import keras
+from keras.datasets import mnist
+# Load the dataset
+# the data, shuffled and split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Extract the features and labels
+features = np.array(np.vstack((x_train,x_test)), 'int16')
+labels = np.array(np.hstack((y_train,y_test)), 'int')
+
+# Extract the hog features
+list_hog_fd = []
+for feature in features:
+    fd = hog(feature.reshape((28, 28)), orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+    list_hog_fd.append(fd)
+hog_features = np.array(list_hog_fd, 'float64')
+
+print("Count of digits in dataset", Counter(labels))
+
+# Create an linear SVM object
+clf = LinearSVC()
+
+# Perform the training
+clf.fit(hog_features, labels)
+
+# Save the classifier
+joblib.dump(clf, "digits_cls.pkl", compress=3)
--- test/test6_ocr_template_milk.py
+++ test/test6_ocr_template_milk.py
@@ -7,11 +7,11 @@ import imutils
 import cv2
 from pytesseract import *
 import keras
-from keras.models import Model,Sequential
+from sklearn.externals import joblib
+from skimage.feature import hog
 
-
-large = cv2.imread('../images/test-digital/test5.jpg')
-rgb = imutils.resize(large, width=1920)#cv2.pyrDown(large)
+large = cv2.imread('../images/test-digital/test_new.jpg')
+rgb = imutils.resize(large, width=1920)  # cv2.pyrDown(large)
 # small = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
 # cv2.imshow("Image1", small)
 # cv2.waitKey(1000)
@@ -25,11 +25,11 @@ rgb = imutils.resize(large, width=1920)#cv2.pyrDown(large)
 
 hsv = cv2.cvtColor(rgb, cv2.COLOR_BGR2HSV)
 threshold_img = np.zeros((hsv.shape[0], hsv.shape[1], 1), dtype=np.uint8)
-cv2.inRange(hsv,np.array([150, 140, 140],np.uint8), np.array([180, 254, 254],np.uint8) , threshold_img)
+cv2.inRange(hsv, np.array([150, 140, 140], np.uint8), np.array([180, 254, 254], np.uint8), threshold_img)
 sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (35, 35))
 threshold_img = cv2.morphologyEx(threshold_img, cv2.MORPH_CLOSE, sqKernel)
-cv2.imshow("Image4", threshold_img)
-cv2.waitKey(1000)
+# cv2.imshow("Image4", threshold_img)
+# cv2.waitKey(1000)
 
 # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (8, 8))
 # grad = cv2.morphologyEx(small, cv2.MORPH_GRADIENT, kernel)
@@ -54,17 +54,17 @@ locs = []
 
 for idx in range(len(cnts)):
     x, y, w, h = cv2.boundingRect(cnts[idx])
-    mask[y:y+h, x:x+w] = 0
+    mask[y:y + h, x:x + w] = 0
     cv2.drawContours(mask, cnts, idx, (255, 255, 255), -1)
-    r = float(cv2.countNonZero(mask[y:y+h, x:x+w])) / (w * h)
+    r = float(cv2.countNonZero(mask[y:y + h, x:x + w])) / (w * h)
 
     if r > 0.5 and w > 50 and h < 50 and h > 20 and y > 280 and y < 500:
-        cv2.rectangle(rgb, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # cv2.rectangle(rgb, (x, y), (x+w, y+h), (0, 255, 0), 2)
         locs.append((x, y, w, h))
 
 # sort the digit locations from left-to-right, then initialize the
 # list of classified digits
-locs = sorted(locs, key=lambda x:x[0])
+locs = sorted(locs, key=lambda x: x[0])
 output = []
 # cv2.imshow('rects', rgb)
 # cv2.waitKey(1000)
@@ -81,10 +81,12 @@ for (i, (gX, gY, gW, gH)) in enumerate(locs):
     # cv2.waitKey(1000)
     group = cv2.threshold(group, 0, 255,
                           cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-    kernel_filter = np.array([[0, -1.1, 0], [-1, 5, -1], [0, -1.1, 0]], np.float32)
-    group = cv2.filter2D(group, -1, kernel=kernel_filter)
+    # kernel_filter = np.array([[0, -1.1, 0], [-1, 5.5, -1], [0, -1.1, 0]], np.float32)
+    # group = cv2.filter2D(group, -1, kernel=kernel_filter)
     kernel = np.ones((2, 2), np.uint8)
-    group = cv2.erode(group, kernel, iterations=3)
+    group = cv2.erode(group, kernel, iterations=2)
+
+
     # rotate func
     def rotate(image, angle, center=None, scale=1.0):
         # get the size
@@ -99,12 +101,14 @@ for (i, (gX, gY, gW, gH)) in enumerate(locs):
         rotated = cv2.warpAffine(image, M, (w, h))
         return rotated
 
-    group = rotate(group,5)
+
+    group = rotate(group, 0)
     # group = cv2.threshold(group, 140, 255, cv2.THRESH_BINARY_INV)[1]
     # group = cv2.Laplacian(group, cv2.CV_64F)
     cv2.imshow('rects1', group)
     cv2.waitKey(1000)
-    print(pytesseract.image_to_string(group,lang='eng', boxes=False,config='-c tessedit_char_whitelist=0123456789 -psm 6'))
+    print(pytesseract.image_to_string(group, lang='eng', boxes=False,
+                                      config='-c tessedit_char_whitelist=0123456789 -psm 6'))
 
     # detect the contours of each individual digit in the group,
     # then sort the digit contours from left to right
@@ -112,10 +116,12 @@ for (i, (gX, gY, gW, gH)) in enumerate(locs):
                                  cv2.CHAIN_APPROX_SIMPLE)
     digitCnts = digitCnts[0] if imutils.is_cv2() else digitCnts[1]
     digitCnts = contours.sort_contours(digitCnts,
-                                   method="left-to-right")[0]
+                                       method="left-to-right")[0]
 
     predicts = []
     model = keras.models.load_model('../images/test-digital/fine-tuned-mnist-weights.h5')
+    # Load the classifier
+    clf = joblib.load("digits_cls.pkl")
     # loop over the digit contours
     for c in digitCnts:
         # compute the bounding box of the individual digit, extract
@@ -123,20 +129,29 @@ for (i, (gX, gY, gW, gH)) in enumerate(locs):
         # the reference OCR-A images
         (x, y, w, h) = cv2.boundingRect(c)
         roi = group[y:y + h, x:x + w]
-        roi = cv2.resize(roi, (60, 90))
-        roi = cv2.copyMakeBorder(roi, 15, 15, 25, 25, cv2.BORDER_CONSTANT, value=[0,0,0])
-        kernel = np.ones((2, 2), np.uint8)
-        roi = cv2.erode(roi, kernel, iterations=5)
+        roi = cv2.copyMakeBorder(roi, 5, 5, 10, 10, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+        # cv2.imshow('rects', roi)
+        # cv2.waitKey(1000)
+        roi = cv2.resize(roi, (90, 120), interpolation=cv2.INTER_NEAREST)
+        roi = cv2.copyMakeBorder(roi, 15, 15, 20, 20, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+        kernel = np.ones((1, 1), np.uint8)
+        roi = cv2.erode(roi, kernel, iterations=2)
         cv2.imshow('rects', roi)
         cv2.waitKey(1000)
-        roi = cv2.resize(roi, (28, 28))
+        roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+        kernel = np.ones((1, 1), np.uint8)
+        roi = cv2.erode(roi, kernel, iterations=2)
         # roi = rotate(roi, 5)
         # roi = cv2.threshold(roi, 10, 255, cv2.THRESH_BINARY_INV)[1]
         cv2.imshow('rects', roi)
         cv2.waitKey(1000)
-        predicts.append(np.array(roi).reshape((28*28)))
+        roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+        nbr = clf.predict(np.array([roi_hog_fd], 'float64'))
+        print(nbr)
+        #native_digital_al(roi)
+        predicts.append(np.array(roi).reshape((28 * 28)))
 
-    predicts = np.asarray(predicts)/255.
+    predicts = np.asarray(predicts) / 255.
     result = model.predict(predicts)
     print('keras: ', np.argmax(result, axis=1))
 
